33d1217bf303f31a2cf4238357b12c2b09f26f0f
Rinse Yaron Schaeffer <rinseyaron@Mac.owner.local>
2026-07-08 17:59:10 +0200

    feat: initial commit

---
+++ src/payload.config.ts
@@ -0,0 +1,33 @@
+import { sqliteD1Adapter } from '@payloadcms/db-d1-sqlite'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { buildConfig } from 'payload'
+import { fileURLToPath } from 'url'
+import sharp from 'sharp'
+
+import { Users } from './collections/Users'
+import { Media } from './collections/Media'
+import migrations from './db/migrations'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  collections: [Users, Media],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: sqliteD1Adapter({
+    binding: cloudflare.env.D1,
+  }),
+  sharp,
+  plugins: [],
+})
